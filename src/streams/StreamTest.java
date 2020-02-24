3172828205fa31a1a91fa85f71267aed4898921a
GauravVarshikar <gaurav.varshikar@gmail.com>
2020-02-25 00:49:39 +0530

    Add files via upload

--- src/streams/StreamTest.java
+++ src/streams/StreamTest.java
@@ -1,7 +1,6 @@
 package streams;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -18,21 +17,37 @@ public class StreamTest {
         List<Student> students = Arrays.asList(student1, student2, student3);
 
 //        Get student with exact match name "jayesh"
+        System.out.println("Get student with exact match name jayesh");
         Optional<Student> s1 = students.stream().filter((s) -> s.getName().equals("Jayesh")).findAny();
         if (s1.isPresent()) System.out.println(s1.get().toString());
         else System.out.println("No student with name Jayesh");
 
 //        Get student with matching address "1235"
+        System.out.println("Get student with matching address 1235");
         Optional<Student> s2 = students.stream().filter(s -> s.getAddress().getZipcode().equals("1235")).findFirst();
         if (s2.isPresent()) System.out.println(s2.get().toString());
         else System.out.println("No student with address 1235");
 
 //        Get all student having mobile numbers 3333.
-        List<Student> s3 = students.stream().filter(s -> s.getMobileNumbers().filter(mobileNumber -> mobileNumber.equals("3333")));
+        System.out.println("Get all student having mobile numbers 3333.");
+        List<Student> s3 = students.stream().filter(s -> {
+            for (MobileNumber mb : s.getMobileNumbers()) {
+                if ("3333".equals(mb.getNumber())) {
+                    return true;
+                }
+            }
+            return false;
+        }).collect(Collectors.toList());
         s3.forEach(System.out::println);
 //        Get all student having mobile number 1233 and 1234
+        System.out.println("Get all student having mobile number 1233 and 1234");
+        List<Student> s4 = students.stream().filter( s-> s.getMobileNumbers().stream().anyMatch(mobileNumber -> mobileNumber.getNumber().equals("1233") || mobileNumber.getNumber().equals("1234"))).collect(Collectors.toList());
+        s4.forEach(System.out :: println);
+
 //        Create a List<Student> from the List<TempStudent>
-//                Convert List<Student> to List<String> of student name
+        System.out.println("Create a List<Student> from the List<TempStudent>");
+//
+//               Convert List<Student> to List<String> of student name
 //        Convert List<students> to String
 //        Change the case of List<String>
 //        Sort List<String>
